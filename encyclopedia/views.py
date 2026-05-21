d057bae474997abda558b4c14a68429fed27758d
Bunny <ashutoshyadav8882049614@gmail.com>
2026-05-22 01:23:13 +0530

    added path route for content

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 
 from . import util
+import markdown2
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+def entry(request, title):
+    md= util.get_entry(title)
+    if md is None:
+        return render(request, "encyclopedia/error.html", {
+            "message": "The requested page was not found."
+        })
+    html=markdown2.markdown(md)
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": html
+    })
